2ee7feab61716c76b514c4b5f5b05d28b187dbf9
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-04-02 22:12:21 +0800

    rewrite query.sh and support -q $i

--- query.sh
+++ query.sh
@@ -1,5 +1,85 @@
 #!/bin/bash
 
+function usage() {
+    echo "usage: $0 [option]
+	-t: tbName  [$tbName]
+	-d: dbName  [$dbName]
+	-q: spcify qX	[all]
+	"
+}
+
+function optParser() {
+    while getopts ":hd:t:q:" opt;do
+	case $opt in
+	    h)
+		usage
+		;;
+	    d)
+		dbName="$OPTARG"
+		;;
+	    t)
+		tbName="$OPTARG"
+	        ;;
+	    q)
+		idx="$OPTARG"
+		start=$idx
+		end=$idx
+	        ;;
+	    \?)
+	        echo "--Invalid args -$OPTARG"
+	        usage
+	        ;;
+	    :)
+		echo "--Need arguement -$OPTARG"
+		usage
+		;;
+	esac
+    done
+}
+
+function doQuery() {
+    start=$1
+    end=$2
+
+    for idx in `seq $start $end`;do
+	query=./sql/q$idx.sql
+	bSuffix=`basename $query`
+	baseName=${bSuffix%%.*}
+	echo "`date +%Y%m%d-%H:%M:%S`,do query:$query"
+	qOutSub="${qOutRoot}/${baseName}"
+	if [ -d $qOutSub ];then
+	    rm -rf $qOutSub
+	fi
+	mkdir -p $qOutSub
+
+	cp -f monitor.sh $qOutRoot
+
+	#start monitor
+	cd $qOutRoot && ./monitor.sh "$baseName-mon"
+	cd - >/dev/null 2>&1
+
+	sql="select count(trip_id) from trips where trip_id < 5;
+	    `cat $query`
+	    "
+	sql=`echo "$sql" | sed "s/from trips\|FROM trips/FROM $tbName/"`
+	#echo "$sql"
+	for i in {1..8};do
+	    resultFile="$qOutSub/$i.txt"
+	    resultFileS="$qOutSub/$i.txt.sort"
+	    escape=`echo "$sql" | $MEGAWISE_HOME/bin/psql -h /tmp -f - -o $resultFile $dbName 2>/dev/null | awk '/Time: / {print $2}'`
+	    sort $resultFile -o $resultFileS
+	    #echo "$i,spend $escape ms"
+	    echo "$escape"
+	done
+
+	#stop monitor
+	cd $qOutRoot && ./monitor.sh
+	cd - >/dev/null 2>&1
+
+	echo
+    done
+}
+
 qOutRoot="./qOut"
 if ! [  -d "${qOutRoot}" ]; then
     mkdir -p ${qOutRoot}
@@ -7,59 +87,12 @@ fi
 
 dbName="postgres"
 tbName="trips"
+start="0"
+end="4"
 
-while [ $# -gt 0 ]; do
-    case "$1" in
-        -d)
-            dbName=$2
-            shift
-            ;;
-        -t)
-            tbName=$2
-            shift
-            ;;
-     	*)
-            echo "---args error----"
-            echo "usage:$0 -t/-d/-n/-a"
-            exit
-            ;;
-    esac
-    shift
-done
+function main() {
+    optParser $@
+    doQuery $start $end
+}
 
-for query in ./sql/q{0,1,2,3,4}.sql;do
-    bSuffix=`basename $query`
-    baseName=${bSuffix%%.*}
-    echo "`date +%Y%m%d-%H:%M:%S`,do query:$baseName"
-    qOutSub="${qOutRoot}/${baseName}"
-    if [ -d $qOutSub ];then
-	rm -rf $qOutSub
-    fi
-    mkdir -p $qOutSub
-
-    cp -f monitor.sh $qOutRoot
-
-    #start monitor
-    cd $qOutRoot && ./monitor.sh "$baseName-mon"
-    cd -
-
-    sql="select count(trip_id) from trips where trip_id < 5;
-`cat $query`
-"
-    #sql=`echo "$sql" | sed "s/from trips\|FROM trips/FROM $tbName/"`
-    #echo "$sql"
-    for i in {1..8};do
-	resultFile="$qOutSub/$i.txt"
-	resultFileS="$qOutSub/$i.txt.sort"
-	escape=`echo "$sql" | $MEGAWISE_HOME/bin/psql -h /tmp -f - -o $resultFile $dbName 2>/dev/null | awk '/Time: / {print $2}'`
-	sort $resultFile -o $resultFileS
-	#echo "$i,spend $escape ms"
-	echo "$escape"
-    done
-
-    #stop monitor
-    cd $qOutRoot && ./monitor.sh
-    cd -
-
-    echo
-done
+main $@
