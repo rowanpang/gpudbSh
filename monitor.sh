0cc7e688b9d532a6b6a3f66b687aa7ec6f64562b
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-04-02 09:16:34 +0800

    update monitor.sh with rcb

--- monitor.sh
+++ monitor.sh
@@ -1,69 +1,213 @@
 #!/bin/bash
 
-pidfile="pid-bg.log"
-if [ -s $pidfile ];then
-    pids=`cat $pidfile`
-    echo "----kill pids: $pids-----"
-    kill $pids
-    rm -rf $pidfile
-    exit
-fi
+function usage () {
+    echo "Usage :  $0 [options] [optIssues]
+        Options:
+        -h		    Display this message
+        -d  dirname	    log dirName
+	-v  verbose level   loglevel [$verbose]
+	-i  interval	    monitor interval [$interval]
+	-c  count	    monitor count [$count]
+    "
+    exit 0
+}
 
-if [ $# -lt 1 ];then
-    echo "--need identifier!!---"
-    exit
-fi
+function cmdChkInstall(){
+    cmd=$1
+    pkg=$cmd
+    [ $# -ge 2 ] && pkg=$2
 
-pids=""
-if [ $1 == '-ui' ];then
-    #upper identify
-    dirName=$2
-fi
+    [ $verbose -ge 1 ] && echo "do cmdChkInstall for $cmd, pkg:$pkg"
 
-if [ -z $dirName ];then
-    nodeName="$HOSTNAME"
+    command -v $cmd >/dev/null 2>&1
+
+    if ! [ $? ];then
+	[ $verbose -ge 1 ] && echo "cmd $cmd not found,do yum install $pkg"
+	yum --assumeyes install  $pkg
+    fi
+}
+
+function optParser(){
+    #init val is based on max run time  half an hour
+    ((count=1800/$interval))
+    while getopts ":hd:v:i:c:" opt;do
+        case $opt in
+            h)
+                usage
+                ;;
+            d)
+        	dirName="$OPTARG"
+                ;;
+	    v)
+		verbose="$OPTARG"
+		;;
+	    i)
+		interval="$OPTARG"
+		#update count
+		((count=(1800+$interval-1)/$interval))
+		;;
+	    c)
+		count="$OPTARG"
+		;;
+	    \?)
+		echo "--Invalid args -$OPTARG"
+		usage
+		;;
+	    :)
+		echo "--Need arguement -$OPTARG"
+		usage
+		;;
+	esac
+    done
+    shift $(($OPTIND-1))
     identify=$1
-    dirName=$nodeName-$identify
-fi
+}
 
-echo "-----log dir:$dirName------"
-if [ -d $dirName ];then
-    rm -rf $dirName
-fi
-mkdir $dirName
+function depCheck(){
+    [ $verbose -ge 1 ] && echo 'in func depCheck'
+    cmdChkInstall dstat
+    cmdChkInstall pidstat sysstat
+    cmdChkInstall lshw
+    cmdChkInstall lsscsi
+    cmdChkInstall ip iproute
+    [ $verbose -ge 1 ] && echo 'out func depCheck'
+}
 
-yum install sysstat dstat
+function doMon(){
+    pids=""
+    #disk
+    recCmd="iostat"
+    iostat -m sd{a..z} sda{a..z} $interval $count > $dirName/disk.log &
+    pids="$pids $recCmd,$!"
 
-#disk
-iostat sdb sdc sdd sde sdf sdg sdh 1 -m > $dirName/disk.log &
-pids="$!"
+    #disk-extra
+    recCmd="iostat"
+    iostat -m -x  sd{a..z} sda{a..z} $interval $count > $dirName/disk.log.extra &
+    pids="$pids $recCmd,$!"
 
-#cpu
-sar -u 1 > $dirName/cpu.log	&
-pids="$pids $!"
+    #cpu
+    recCmd="sar"
+    sar -u $interval $count > $dirName/cpu.log	&
+    pids="$pids $recCmd,$!"
 
-sar -P ALL 1 > $dirName/cpuPer.log 	&
-pids="$pids $!"
+    #mem
+    recCmd="free"
+    free -c $count -s $interval -h > $dirName/mem.log &
+    pids="$pids $recCmd,$!"
 
-#mem
-free -c 3600 -s 1 -h > $dirName/mem.log &
-pids="$pids $!"
+    recCmd="pidstat"
+    #pidstat
+    pidstat -l -t -d -u -C "megawise_server|postgres" $interval $count > $dirName/pidstat.log &
+    pids="$pids $recCmd,$!"
 
-#pidstat
-pidstat -l -t -d -u -C "postgres" 1 > $dirName/pidstat.log &
-pids="$pids $!"
+:<<EOF
+    top:
+	-d: interval
+	-n: count
+	-b: batch mode
+	-i: skip idle process
+	-c: command show
+	-w: wild show	#confilict with -o
+	-o: sort by
+EOF
+    recCmd="top"
+    COLUMNS=167 top -d $interval -n $count -b -i -c -o RES > $dirName/top.log &
+    pids="$pids $recCmd,$!"
 
-#dstat
-dstat --nocolor > $dirName/dstat.log &
-pids="$pids $!"
+    #nvidia-pmon
+    recCmd="nvidia-smi"
+    nvidia-smi pmon > $dirName/nvidia-pmon.log      &
+    pids="$pids $recCmd,$!"
 
-#nvidia-pmon
-nvidia-smi pmon > $dirName/nvidia-pmon.log	&
-pids="$pids $!"
+    #nvidia-dmon
+    recCmd="nvidia-smi"
+    nvidia-smi dmon > $dirName/nvidia-dmon.log      &
+    pids="$pids $recCmd,$!"
 
-#nvidia-dmon
-nvidia-smi dmon > $dirName/nvidia-dmon.log	&
-pids="$pids $!"
+    #nvidia-smi -lms 500
+    recCmd="nvidia-smi"
+    nvidia-smi -lms 500 > $dirName/nvidia-lms.log      &
+    pids="$pids $recCmd,$!"
 
-echo $pids > $pidfile
-echo "-----bg pids:$pids----------------"
+
+    echo $pids > $pidfile
+    [ $verbose -ge 1 ] && echo "-----bg pids:$pids----------------"
+}
+
+function doInit() {
+    if [ -z $dirName ];then
+	if [ -z $identify ];then
+	    echo "--need identifier!! exit 1---"
+	    exit 1
+	fi
+	nodeName="$HOSTNAME"
+	dirName=$nodeName-$identify
+    fi
+
+    if [ $verbose -ge 1 ];then
+	echo "idt:$identify,dir:$dirName,verbose:$verbose"
+	echo "-----log dir:$dirName------"
+    fi
+
+    [ -d $dirName ] && rm -rf $dirName
+    mkdir $dirName
+}
+
+function checkKill() {
+    if [ -s $pidfile ];then
+	pidCmds=`cat $pidfile`
+	[ $verbose -ge 1 ] && echo "----try kill: $pidCmds-----"
+
+	for pidCmd in $pidCmds;do
+	    pid=${pidCmd#*,}
+	    oCmd=${pidCmd%,*}
+	    [ $verbose -ge 1 ] && echo -ne "\t kill pid-oCmd: $pid-$oCmd---"
+	    cCmd=`ps -o pid,command $pid 2>/dev/null | awk '{if (NR>1) print $2}'`
+
+	    if [ X$cCmd != X ];then
+		match=`echo $cCmd | grep -c $oCmd`
+		if [ $match -ge 1 ] ;then
+		    kill $pid
+		    [ $verbose -ge 1 ] && echo "ok"
+		else
+		    [ $verbose -ge 1 ] && echo "not math cCmd $cCmd,skip"
+		fi
+	    fi
+	done
+
+	rm -rf $pidfile
+	exit
+    fi
+}
+
+function gatherInfo(){
+    pfx="info-"
+    lsscsi > $dirName/${pfx}lsscsi.log
+    df -h > $dirName/${pfx}df.log
+
+    cat /etc/os-release > $dirName/${pfx}osInfo.log
+    echo >> $dirName/${pfx}osInfo.log
+    uname -a >> $dirName/${pfx}osInfo.log
+
+    lshw > $dirName/${pfx}lshw.log
+
+    lscpu > $dirName/${pfx}lscpu.log
+
+    ip a > $dirName/${pfx}ipA.log
+}
+
+function main(){
+    optParser $@
+    checkKill
+    doInit
+    depCheck
+    gatherInfo
+    doMon
+}
+
+verbose="1"
+pidfile="pid-bg.log"
+interval=1
+count=""
+
+main $@
